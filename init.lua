67bcf015e532d9e944d9a175efdb272d322926a6
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-23 15:07:22 -0400

    removed old lsp lua

--- init.lua
+++ init.lua
@@ -12,8 +12,6 @@ require('plugins/nvim-cmp')
 require("plugins/telescope")
 require('plugins/nvim-treesitter')
 require("plugins/comment")
-
 require("plugins/lsp")
 require("plugins/autopairs")
 
--- require('plugins/nvim-lspconfig')
