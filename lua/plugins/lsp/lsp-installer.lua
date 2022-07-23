56fe31004e0dedfa6f92fc20b9c01faedb5b6b20
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-23 14:25:11 -0400

    improved lsp

--- lua/plugins/lsp/lsp-installer.lua
+++ lua/plugins/lsp/lsp-installer.lua
@@ -3,33 +3,22 @@ if not status_ok then
 	return
 end
 
--- Register a handler that will be called for all installed servers.
--- Alternatively, you may also register handlers on specific server instances instead (see example below).
-lsp_installer.on_server_ready(function(server)
+local lspconfig = require("lspconfig")
+
+local servers = { 'html', 'cssls', 'tsserver', "sumneko_lua", "jsonls", "tailwindcss" }
+
+lsp_installer.setup({
+	ensure_installed = servers,
+})
+
+for _, server in pairs(servers) do
 	local opts = {
 		on_attach = require("plugins.lsp.handlers").on_attach,
 		capabilities = require("plugins.lsp.handlers").capabilities,
 	}
-
-	 if server.name == "jsonls" then  -- make json lsp smarter
-	 	local jsonls_opts = require("plugins.lsp.settings.jsonls")
-	 	opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
-	 end
-
-	 if server.name == "sumneko_lua" then   -- make lua know vim
-	 	local sumneko_opts = require("plugins.lsp.settings.sumneko_lua")
-	 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-	 end
-
-   if server.name == "tailwindcss" then
-    local tailwind_opts = require("plugins.lsp.settings.tailwindcss")
-    opts = vim.tbl_deep_extend("force", tailwind_opts, opts)
-   end
-
-
-	-- This setup() function is exactly the same as lspconfig's setup function.
-	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-	server:setup(opts)
-end)
-
-
+	local has_custom_opts, server_custom_opts = pcall(require, "plugins.lsp.settings." .. server)
+	if has_custom_opts then
+		opts = vim.tbl_deep_extend("force", opts, server_custom_opts)
+	end
+	lspconfig[server].setup(opts)
+end
