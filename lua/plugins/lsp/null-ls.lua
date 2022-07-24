c2c4d8fcfe738c384aa928ab1f45fe85d35a2b5b
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-24 02:57:19 -0400

    added new null builtins

--- lua/plugins/lsp/null-ls.lua
+++ lua/plugins/lsp/null-ls.lua
@@ -1,17 +1,21 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  return
+	return
 end
 
 local formatting = null_ls.builtins.formatting
--- local diagnostics = null_ls.builtins.diagnostics
+local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
+local completion = null_ls.builtins.completion
 
 null_ls.setup({
-  debug = true,
-  sources = {
-    formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-    formatting.stylua,
-    code_actions.eslint
-  },
+	debug = true,
+	sources = {
+		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+		formatting.stylua, -- lua
+		code_actions.eslint, -- js/ts
+		completion.spell, -- spell
+		diagnostics.stylelint, -- css
+		formatting.rustywind, -- tailwind
+	},
 })
