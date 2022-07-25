55afb5da4701688ebfcf604ab617063c1a47090f
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-25 11:50:18 -0400

    commented out the word completion

--- lua/plugins/lsp/null-ls.lua
+++ lua/plugins/lsp/null-ls.lua
@@ -6,15 +6,15 @@ end
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
-local completion = null_ls.builtins.completion
+-- local completion = null_ls.builtins.completion
 
 null_ls.setup({
 	debug = true,
 	sources = {
-		formatting.prettier.with({ extra_args = { "--single-quote", "--jsx-single-quote" } }),
+		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.stylua, -- lua
 		code_actions.eslint, -- js/ts
-		completion.spell, -- spell
+		-- completion.spell, -- spell
 		diagnostics.stylelint, -- css
 		formatting.rustywind, -- tailwind
 	},
