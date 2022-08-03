ec60ec1c786a4b1d1fd78c173fd607490c7caaa5
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-03 03:20:58 -0400

    removed not working esc keymap

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,58 +1,62 @@
 -----------------------------------------------------------
 -- Autocommand functions
 -----------------------------------------------------------
--- Define autocommands with Lua APIs
--- See: h:api-autocmd, h:augroup
 local augroup = vim.api.nvim_create_augroup -- Create/get autocommand group
 local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
 
 -- Highlight on yank
 augroup("YankHighlight", {
-    clear = true
+	clear = true,
 })
 
 autocmd("TextYankPost", {
-    group = "YankHighlight",
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = "IncSearch",
-            timeout = "200"
-        })
-    end
+	group = "YankHighlight",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = "200",
+		})
+	end,
 })
 
 -- Remove whitespace on save
 autocmd("BufWritePre", {
-    pattern = "*",
-    command = ":%s/\\s\\+$//e"
+	pattern = "*",
+	command = ":%s/\\s\\+$//e",
 })
 
 -- Don't auto commenting new lines
 autocmd("BufEnter", {
-    pattern = "*",
-    command = "set fo-=c fo-=r fo-=o"
+	pattern = "*",
+	command = "set fo-=c fo-=r fo-=o",
 })
 
 -- Settings for filetypes:
 -- Disable line length marker
 augroup("setLineLength", {
-    clear = true
+	clear = true,
 })
 autocmd("Filetype", {
-    group = "setLineLength",
-    pattern = {"text", "markdown", "html", "xhtml", "javascript", "typescript"},
-    command = "setlocal cc=0"
+	group = "setLineLength",
+	pattern = { "text", "markdown", "html", "xhtml", "javascript", "typescript" },
+	command = "setlocal cc=0",
 })
 
 -- Save folds on exit and load on start
 augroup("AutoSaveFolds", {})
 autocmd("BufWinLeave", {
-    group = "AutoSaveFolds",
-    pattern = "*",
-    command = "silent! mkview"
+	group = "AutoSaveFolds",
+	pattern = "*",
+	command = "silent! mkview",
 })
 autocmd("BufWinEnter", {
-    group = "AutoSaveFolds",
-    pattern = "*",
-    command = "silent! loadview"
+	group = "AutoSaveFolds",
+	pattern = "*",
+	command = "silent! loadview",
+})
+
+-- Turn off line numbers in terminal
+autocmd("TermOpen", {
+	pattern = "*",
+	command = "setlocal nonumber norelativenumber",
 })
