5c70231df005447baf0b30a05a9dde073bf82265
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-03 03:21:42 -0400

    removed unused capabilities

--- lua/plugins/lsp/handlers.lua
+++ lua/plugins/lsp/handlers.lua
@@ -86,22 +86,7 @@ local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_ok then
 	return
 end
-capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
 
-capabilities.textDocument.completion.completionItem.documentationFormat = { "markdown", "plaintext" }
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities.textDocument.completion.completionItem.preselectSupport = true
-capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
-capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
-capabilities.textDocument.completion.completionItem.deprecatedSupport = true
-capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
-capabilities.textDocument.completion.completionItem.tagSupport = {
-	valueSet = { 1 },
-}
-capabilities.textDocument.completion.completionItem.resolveSupport = {
-	properties = { "documentation", "detail", "additionalTextEdits" },
-}
-
-M.capabilities = capabilities
+M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
 
 return M
