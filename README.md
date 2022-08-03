76f371f31c566f4b74a5f68ae11d33239f59dc09
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-03 03:22:39 -0400

    simple styling

--- README.md
+++ README.md
@@ -3,12 +3,14 @@
 - Hello, I wanted to share a minimal neovim config for JS developers.
 
 - Capabilities:
+
   - lsp + auto-complete + snippets
   - formatting + format on save
   - telescope == fzf
 
 - The keymappings are heavily opinionated.
 - I made it using:
+
   - <https://github.com/brainfucksec/neovim-lua>
   - <https://github.com/LunarVim/Neovim-from-scratch/>
 
--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -3,63 +3,56 @@
 ----------------------------------------------------------
 local telescope_status_ok, telescope = pcall(require, "telescope")
 if not telescope_status_ok then
-	return
+    return
 end
 
 local actions_status_ok, actions = pcall(require, "telescope.actions")
 if not actions_status_ok then
-	return
+    return
 end
 
 telescope.setup({
 
-	defaults = {
-		entry_prefix = "  ", -- remove symbols
-		prompt_prefix = "  ",
-		selection_caret = "  ",
+    defaults = {
+        entry_prefix = "  ", -- remove symbols
+        prompt_prefix = "  ",
+        selection_caret = "  ",
 
-		mappings = {
-			i = {
-				["<C-u>"] = false, -- clear input with C-u
-				["<C-p>"] = actions.close, -- exit
-				["<C-j>"] = actions.move_selection_next, -- next item
-				["<C-k>"] = actions.move_selection_previous, -- previous item
-				["<C-?>"] = actions.which_key, -- available keys
-			},
-			n = {
-				["<C-?>"] = actions.which_key, -- see options
-			},
-		},
+        mappings = {
+            i = {
+                ["<C-u>"] = false, -- clear input with C-u
+                ["<C-p>"] = actions.close, -- exit
+                ["<C-j>"] = actions.move_selection_next, -- next item
+                ["<C-k>"] = actions.move_selection_previous, -- previous item
+                ["<C-?>"] = actions.which_key -- available keys
+            },
+            n = {
+                ["<C-?>"] = actions.which_key -- see options
+            }
+        },
 
-		file_ignore_patterns = { -- ignore these files
-			"node_modules/.*",
-			".git/.*",
-		},
-		layout_strategy = "flex", -- display: flex;
-	},
-	pickers = { -- defining the options of different pickers
-		find_files = {
-			theme = "dropdown",
-			previewer = false,
-			hidden = true,
-			find_command = { "fd", "--type", "f", "--strip-cwd-prefix" }, -- turn off "./"
-		},
-
-		oldfiles = {
-			theme = "dropdown",
-			previewer = false,
-		},
-		current_buffer_fuzzy_find = {
-			theme = "ivy",
-			previewer = false,
-		},
-		live_grep = {
-			theme = "ivy",
-		},
-	},
-	extensions = {
-		file_browser = require("plugins.telescope.file-browser"),
-	},
+        file_ignore_patterns = { -- ignore these files
+        "node_modules/.*", ".git/.*"},
+        layout_strategy = "flex" -- display: flex;
+    },
+    pickers = { -- defining the options of different pickers
+        find_files = {
+            theme = "dropdown",
+            previewer = false,
+            hidden = true,
+            find_command = {"fd", "--type", "f", "--strip-cwd-prefix"} -- turn off "./"
+        },
+        current_buffer_fuzzy_find = {
+            theme = "ivy",
+            previewer = false
+        },
+        live_grep = {
+            theme = "ivy"
+        }
+    },
+    extensions = {
+        file_browser = require("plugins.telescope.file-browser")
+    }
 })
 
 telescope.load_extension("file_browser")
