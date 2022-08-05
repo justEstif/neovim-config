c985e8e72c444c371bdc3086e60c42dc949954c7
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-04 20:58:50 -0400

    removed unused lsp keymaps and on save format

--- lua/plugins/lsp/handlers.lua
+++ lua/plugins/lsp/handlers.lua
@@ -51,10 +51,7 @@ end
 local function lsp_keymaps(bufnr)
 	local opts = { noremap = true, silent = true }
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gl", "<cmd>lua vim.lsp.diagnostic.get_line_diagnostics()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>d", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>f", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>h", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
@@ -62,16 +59,6 @@ local function lsp_keymaps(bufnr)
 end
 
 M.on_attach = function(client, bufnr)
-	-- -- format on save
-	-- if client.supports_method("textDocument/formatting") then
-	-- 	vim.api.nvim_create_autocmd("BufWritePre", {
-	-- 		buffer = bufnr,
-	-- 		callback = function()
-	-- 			vim.lsp.buf.format({ bufnr = bufnr })
-	-- 		end,
-	-- 	})
-	-- end
-
 	-- add lsp that has builtin formatters in order to use null-ls instead
 	local ignored_formatters = { "tsserver", "sumneko_lua", "html" }
 	for _, value in ipairs(ignored_formatters) do
