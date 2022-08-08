7e09cd46020a3f6a606dd82cbc5e69a948db8efc
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-08 15:25:26 -0400

    remaped " -> ' for jumping to mark

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -30,6 +30,7 @@ map({ "n", "v" }, "L", "$") -- L to go to the end of line(n)
 map({ "n", "v" }, "J", "}") -- J to jump previous blocks(n)
 map({ "n", "v" }, "K", "{") -- L to jump next blocks(n)
 map({ "n", "v" }, "'", "%") -- ' to jump to matching closer
+map({ "n", "v" }, '"', "'") -- ' to jump to mark
 
 map("n", "<A-j>", ":m .+1<CR>==") -- move line up(n)
 map("n", "<A-k>", ":m .-2<CR>==") -- move line down(n)
