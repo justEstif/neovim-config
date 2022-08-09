7d54363822c2902e127c1021034893999a874715
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-09 04:22:43 -0400

    using one nord colors

--- lua/core/statusline.lua
+++ lua/core/statusline.lua
@@ -7,7 +7,28 @@ if not status_ok then
 	return
 end
 
-local colors = require("onenord.colors").load()
+local colors = {
+	fg = "#C8D0E0",
+	fg_light = "#E5E9F0",
+	bg = "#2E3440",
+	gray = "#646A76",
+	light_gray = "#6C7A96",
+	cyan = "#88C0D0",
+	blue = "#81A1C1",
+	dark_blue = "#5E81AC",
+	green = "#A3BE8C",
+	light_green = "#8FBCBB",
+	dark_red = "#BF616A",
+	red = "#D57780",
+	light_red = "#DE878F",
+	pink = "#E85B7A",
+	dark_pink = "#E44675",
+	orange = "#D08F70",
+	yellow = "#EBCB8B",
+	purple = "#B988B0",
+	light_purple = "#B48EAD",
+	none = "NONE",
+}
 
 local vi_mode_colors = {
 	NORMAL = colors.cyan,
