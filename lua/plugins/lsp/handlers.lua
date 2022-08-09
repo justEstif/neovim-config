c6cce92693eef72c2df274bce59ea38ce4c27313
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-09 10:31:52 -0400

    minor styling

--- lua/plugins/lsp/handlers.lua
+++ lua/plugins/lsp/handlers.lua
@@ -60,6 +60,9 @@ local function lsp_keymaps(bufnr)
 end
 
 M.on_attach = function(client, bufnr)
+	lsp_keymaps(bufnr)
+	lsp_highlight_document(client)
+
 	-- add lsp that has builtin formatters in order to use null-ls instead
 	local ignored_formatters = { "tsserver", "sumneko_lua", "html" }
 	for _, value in ipairs(ignored_formatters) do
@@ -67,9 +70,6 @@ M.on_attach = function(client, bufnr)
 			client.server_capabilities.document_formatting = false
 		end
 	end
-
-	lsp_keymaps(bufnr)
-	lsp_highlight_document(client)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
