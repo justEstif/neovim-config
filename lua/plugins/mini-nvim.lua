4cb312ee1889103780d7d472d5d2b3bdcd393fb5
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-11 16:50:51 -0400

    implement mini jump with f/t

---
+++ lua/plugins/mini-nvim.lua
@@ -0,0 +1,15 @@
+-----------------------------------------------------------
+-- Mini configuration file
+----------------------------------------------------------
+
+local status_jump, mini_jump = pcall(require, "mini.jump")
+if not status_jump then
+	return
+end
+
+mini_jump.setup({
+	delay = {
+		highlight = 150,
+		idle_stop = 10000000,
+	},
+})
