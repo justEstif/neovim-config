8a1b7a9ca7c250a7b0a134541272a2df780a4551
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-15 08:43:28 -0400

    using fzf extension for improved search

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -79,5 +79,14 @@ telescope.setup({
 			},
 		},
 	},
-	extensions = {},
+	extensions = {
+		fzf = {
+			fuzzy = true,
+			override_genearic_sorter = false,
+			override_file_sorter = true,
+			case_mode = "smart_case",
+		},
+	},
 })
+
+require("telescope").load_extension("fzf")
