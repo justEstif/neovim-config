a207552e0bfb956223c694acd3fe9788493bcfea
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-22 10:03:49 -0400

    Changed sneak s keymap to uppercase S

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -86,7 +86,7 @@ map("n", "ms", ":Telescope current_buffer_fuzzy_find<CR>") -- find text in curre
 map("n", "mS", ":Telescope live_grep<CR>") -- find text in directory
 map("n", "<C-p>", ":Telescope find_files<CR>") -- find file
 map("n", "<C-n>", ":Telescope file_browser<CR>") -- open/close file browser
-map({"n", "v"}, 's', ":HopChar2<CR>") -- sneak s
+map({"n", "v"}, 'S', ":HopChar2<CR>") -- sneak s
 -- other keybindings in:
 -- plugins.lsp/handlers
 -- plugins.nvim-cmp
