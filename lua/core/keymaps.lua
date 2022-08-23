45c743f4a42fc999f5f78e5f01faa795b7630f7c
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-23 00:36:08 -0400

    Organized keymaps to better format

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -20,19 +20,21 @@ vim.g.mapleader = ","
 -----------------------------------------------------------
 
 -- disable keys
-local disable_keys = { "<up>", "<left>", "<down>", "<right>", "gh", "gl", "gL", "<C-u>", "<C-b>"}
+local disable_keys = { "<up>", "<left>", "<down>", "<right>", "gh", "gl", "gL", "<C-u>", "<C-b>" }
 for _, disable_key in pairs(disable_keys) do
 	map("", disable_key, "<nop>")
 end
 
+-- Custom Navigation
 map({ "n", "v" }, '"', "%") -- " to jump to matching closer
 map({ "n", "v" }, "H", "_") -- H to go the start of line(n)
 map({ "n", "v" }, "L", "$") -- L to go to the end of line(n)
 map({ "n", "v" }, "K", "{") -- L to jump next blocks(n)
 map({ "n", "v" }, "J", "}") -- J to jump previous blocks(n)
 map({ "n", "v" }, "<leader>j", "gJ") -- leader j to join lines(n)
-map({ "n", "v" }, "<C-f>", "<C-u>") -- move half up
+map({ "n", "v" }, "<C-f>", "<C-u><Esc>") -- move half up/turn off f
 
+-- Lines
 map("n", "<A-j>", ":m .+1<CR>==") -- move line up(n)
 map("n", "<A-k>", ":m .-2<CR>==") -- move line down(n)
 map("v", "<A-j>", ":m '>+1<CR>gv=gv") -- move line up(v)
@@ -49,6 +51,7 @@ map("n", "<leader>tk", ":tabclose<CR>") -- kill current tab
 map("n", "<leader>bn", ":bn<CR>") -- next buffer(n)
 map("n", "<leader>bp", ":bp<CR>") -- previous buffer(n)
 map("n", "<leader>bk", ":lua MiniBufremove.delete()<cr>") -- delete current buffer
+map("n", "<leader>q", ":bufdo bdelete<CR>:Alpha<CR>") -- kill all buffers
 
 -- Window
 map("n", "<C-k>", "<C-w>k") -- window up
@@ -62,19 +65,17 @@ map("n", "<C-Down>", ":resize +2<CR>")
 map("n", "<C-Left>", ":vertical resize +2<CR>")
 map("n", "<C-Right>", ":vertical resize -2<CR>")
 
-map("v", "p", '"_dP') -- keep the yanked when pasting in visual mode
-
-map("t", "<Esc>", "<C-\\><C-n>") -- esc exit terminal
-
+-- Indent
 map("n", ">", ">>") -- quick indent
 map("n", "<", "<<") -- quick indent
 map("v", "<", "<gv") -- stay in indent mode
 map("v", ">", ">gv") -- stay in indent mode
 
+-- Others
+map("v", "p", '"_dP') -- keep the yanked when pasting in visual mode
+map("t", "<Esc>", [[<C-\><C-n>]]) -- esc exit terminal
 map("n", "<Esc>", ":nohl<CR>") -- clear search highlights
-map("n", "<leader>q", ":bufdo bdelete<CR>:Alpha<CR>") -- kill all buffers
 map("n", "<leader>r", ":so %<CR>") -- Reload configuration without restart nvim
-
 -- open link under cursor
 map("n", "gx", "<cmd>silent execute '!open ' . shellescape('<cWORD>')<CR>")
 
@@ -82,12 +83,16 @@ map("n", "gx", "<cmd>silent execute '!open ' . shellescape('<cWORD>')<CR>")
 -- Applications and Plugins shortcuts
 -----------------------------------------------------------
 
-map("n", "ms", ":Telescope current_buffer_fuzzy_find<CR>") -- find text in current file
+-- Hop
+map({ "n", "v" }, "ss", ":HopChar2<CR>") -- sneak s
+
+-- Telescope
 map("n", "mS", ":Telescope live_grep<CR>") -- find text in directory
+map("n", "<C-b>", ":Telescope buffers<CR>") -- list all buffers
 map("n", "<C-p>", ":Telescope find_files<CR>") -- find file
 map("n", "<C-n>", ":Telescope file_browser<CR>") -- open/close file browser
-map("n", "<C-b>", ":Telescope buffers<CR>") -- list all buffers
-map({ "n", "v" }, "ss", ":HopChar2<CR>") -- sneak s
+map("n", "ms", ":Telescope current_buffer_fuzzy_find<CR>") -- find text in current file
+
 -- other keybindings in:
 -- plugins.lsp/handlers
 -- plugins.nvim-cmp
