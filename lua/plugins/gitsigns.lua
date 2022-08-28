e31c0b291685c45a854dfb683249b59006669556
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-28 06:40:43 -0400

    🔥 Move gitsigns keybindings to keymaps

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -7,22 +7,4 @@ if not status_ok then
 	return
 end
 
-gitsigns.setup({
-	on_attach = function(bufnr)
-		local gs = package.loaded.gitsigns
-
-		local function map(mode, l, r, opts)
-			opts = opts or {}
-			opts.buffer = bufnr
-			vim.keymap.set(mode, l, r, opts)
-		end
-
-		-- Actions
-		map({ "n", "v" }, "<leader>gs", ":Gitsigns stage_hunk<CR>")
-		map({ "n", "v" }, "<leader>gr", ":Gitsigns reset_hunk<CR>")
-		map("n", "<leader>gS", gs.stage_buffer)
-		map("n", "<leader>gu", gs.undo_stage_hunk)
-		map("n", "<leader>gR", gs.reset_buffer)
-		map("n", "<leader>gd", gs.diffthis)
-	end,
-})
+gitsigns.setup({})
