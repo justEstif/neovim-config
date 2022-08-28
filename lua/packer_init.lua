fed9465c6baace797fd1efce8a5ba806254cec36
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-28 06:37:47 -0400

    ➕ Add gitmoji extension to telescope

--- lua/packer_init.lua
+++ lua/packer_init.lua
@@ -54,6 +54,7 @@ return packer.startup(function(use)
 	use({
 		"nvim-telescope/telescope.nvim", -- telescope
 		"nvim-telescope/telescope-file-browser.nvim", -- telescope file browser
+		"olacin/telescope-gitmoji.nvim", -- gitmoji
 		branch = "0.1.x",
 		requires = "nvim-lua/plenary.nvim",
 	})
--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -97,3 +97,4 @@ telescope.setup({
 	},
 })
 telescope.load_extension("file_browser")
+telescope.load_extension("gitmoji")
