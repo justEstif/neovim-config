d6608b89aaab944bc46385ac57ce187e3900cf6d
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-30 05:51:12 -0400

    Added neogit configuration

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,13 @@
+-----------------------------------------------------------
+-- Neogit configuration file
+----------------------------------------------------------
+
+local status, neogit = pcall(require, "neogit")
+if not status then
+	return
+end
+
+neogit.setup({
+  disable_commit_confirmation = true,
+})
+
