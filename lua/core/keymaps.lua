4fa9d27417ace8bf95c4d1179e4252159e898176
justEstif <95101485+justEstif@users.noreply.github.com>
2022-09-15 11:57:28 -0400

    feat: add trouble todo keymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -82,6 +82,7 @@ map("n", "<C-b>", ":Telescope buffers<CR>") -- list all open buffers
 map("n", "<C-p>", ":Telescope find_files<CR>") -- find file in dir
 map("n", "<C-n>", ":Telescope file_browser<CR>") -- open/close file browser
 map("n", "mS", ":Telescope live_grep<CR>") -- find text in current dir
+map("n", "mt", ":TodoTrouble<CR>") -- open todo list
 
 -- other keybindings in:
 -- plugins.lsp/handlers
